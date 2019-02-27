dc5de3499f03b4686e341447ef8ff3f36e91ed55
George Liu <eva2000@centminmod.com>
2019-02-28 01:35:40 +1000

    initial commit

---
+++ get-php-ver.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#################################################
+# get latest php version tags for each branch
+#################################################
+phpversions='/tmp/phpversions.txt'
+
+
+getversions() {
+echo > "$phpversions"
+curl -s "https://api.github.com/repos/php/php-src/tags?per_page=500" | jq -r '.[].name' | grep 'php-' | egrep -iv 'alpha|beta|rc' >> "$phpversions"
+curl -s "https://api.github.com/repos/php/php-src/tags?page=2&per_page=500" | jq -r '.[].name' | grep 'php-' | egrep -iv 'alpha|beta|rc' >> "$phpversions"
+curl -s "https://api.github.com/repos/php/php-src/tags?page=3&per_page=500" | jq -r '.[].name' | grep 'php-' | egrep -iv 'alpha|beta|rc' >> "$phpversions"
+curl -s "https://api.github.com/repos/php/php-src/tags?page=4&per_page=500" | jq -r '.[].name' | grep 'php-' | egrep -iv 'alpha|beta|rc' >> "$phpversions"
+curl -s "https://api.github.com/repos/php/php-src/tags?page=5&per_page=500" | jq -r '.[].name' | grep 'php-' | egrep -iv 'alpha|beta|rc' >> "$phpversions"
+curl -s "https://api.github.com/repos/php/php-src/tags?page=6&per_page=500" | jq -r '.[].name' | grep 'php-' | egrep -iv 'alpha|beta|rc' >> "$phpversions"
+curl -s "https://api.github.com/repos/php/php-src/tags?page=7&per_page=500" | jq -r '.[].name' | grep 'php-' | egrep -iv 'alpha|beta|rc' >> "$phpversions"
+curl -s "https://api.github.com/repos/php/php-src/tags?page=8&per_page=500" | jq -r '.[].name' | grep 'php-' | egrep -iv 'alpha|beta|rc' >> "$phpversions"
+curl -s "https://api.github.com/repos/php/php-src/tags?page=9&per_page=500" | jq -r '.[].name' | grep 'php-' | egrep -iv 'alpha|beta|rc' >> "$phpversions"
+curl -s "https://api.github.com/repos/php/php-src/tags?page=10&per_page=500" | jq -r '.[].name' | grep 'php-' | egrep -iv 'alpha|beta|rc' >> "$phpversions"
+cat "$phpversions" | grep -m1 '7.3.' | sed -e 's|php-||'
+cat "$phpversions" | grep -m1 '7.2.' | sed -e 's|php-||'
+cat "$phpversions" | grep -m1 '7.1.' | sed -e 's|php-||'
+cat "$phpversions" | grep -m1 '7.0.' | sed -e 's|php-||'
+cat "$phpversions" | grep -m1 '5.6.' | sed -e 's|php-||'
+cat "$phpversions" | grep -m1 '5.5.' | sed -e 's|php-||'
+cat "$phpversions" | grep -m1 '5.4.' | sed -e 's|php-||'
+#cat "$phpversions" | grep -m1 '5.3.' | sed -e 's|php-||'
+}
+
+getversions
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Get PHP versions via Github.com API
+
+Get the latest PHP version tags for each major branch via Github v3 API.
+
+```
+./get-php-ver.sh 
+7.3.2
+7.2.15
+7.1.26
+7.0.33
+5.6.40
+5.5.38
+5.4.45
+```
